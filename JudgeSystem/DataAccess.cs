0ec866b56e5e1944d0fe69c1abb1f3557d252b7d
Nasim Uddin <23162353+thenasim@users.noreply.github.com>
2021-08-16 00:52:06 +0600

    DataAccess and Helper class

---
+++ JudgeSystem/DataAccess.cs
@@ -0,0 +1,35 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace JudgeSystem
+{
+    public static class DataAccess
+    {
+        private static readonly SqlConnection _sqlConnection = new SqlConnection(Helper.CnnVal("DBConnection"));
+
+        public static SqlConnection SqlCon
+        {
+            get
+            {
+                if (_sqlConnection.State == ConnectionState.Closed)
+                {
+                    _sqlConnection.Open();
+                }
+
+                return _sqlConnection;
+            }
+        }
+
+        public static SqlDataReader GetData(string queryString)
+        {
+            SqlCommand cmd = new SqlCommand(queryString, SqlCon);
+            return cmd.ExecuteReader();
+        }
+
+        public static int UpdateDelete(string queryString)
+        {
+            SqlCommand cmd = new SqlCommand(queryString, SqlCon);
+            return cmd.ExecuteNonQuery();
+        }
+    }
+}
